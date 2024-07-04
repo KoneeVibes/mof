372778efa0e816dc5364621f2cc36c1ca28b31a6
Konee <104918313+KoneeVibes@users.noreply.github.com>
2024-07-04 13:38:29 +0100

    handled image height in login page

--- src/pages/auth/styled.js
+++ src/pages/auth/styled.js
@@ -1,15 +1,27 @@
 import styled from "styled-components";
+import { Row } from "../../components/flex";
 
 export const AuthWrapper = styled("div")(() => ({
+    height: "100vh",
+    overflow: "hidden",
+    "@media screen and (max-width: 1024px)": {
+        height: "auto",
+    },
     ".auth-img-div": {
         flex: 1,
-        "svg": {
+        "img": {
             objectFit: "cover",
             width: "100%",
-            height: "auto",
+            height: "100%",
         }
     },
     ".auth-form-div": {
         flex: 1
-    }
+    },
 }))
+
+export const AuthRow = styled(Row)(() => {
+    return {
+        height: "inherit"
+    }
+}) 
