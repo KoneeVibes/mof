b7c0eac56ef4890775ce20c26dddb962a81ff248
Ibukun <ibukunoladiporaji@gmail.com>
2024-07-05 14:51:32 +0100

    update to the login html

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -15,9 +15,9 @@ export const Auth = () => {
                     <form>
                         {/* login form hTML should begin below this line.  */}
                         <input type="email" name="email" placeholder="Email" required /> <br/>
-                        <input type="password" name="password" placeholder="Password" required /> <br/>
+                        <input type="password" name="password" placeholder="Password" required />
                          <input type="checkbox"/>
-                         <label for="demoCheckbox"> Check me!</label><br/>
+                         <label for="demoCheckbox"> Show</label><br/>
                         <a href="/" class="forgot-password">Forgot Password?</a> <br/> 
                         <BaseButton>LOG IN</BaseButton>
                     </form>
