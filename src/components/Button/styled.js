031c8cf54c995bb3d2931f582838a48e53fb3d44
Newton <ephraimnewton533@gmail.com>
2024-07-08 08:25:48 +0100

    styling for the buttons

--- src/components/Button/styled.js
+++ src/components/Button/styled.js
@@ -4,12 +4,16 @@ export const BaseButton = styled.button(
     ({direction}) => {
         return{
             //styles for button should begin below this line.
-            
-
-
-
-
-
+            textAlign: "center",
+            backgroundColor: "#059212",
+            border: "none",
+            color: "white",
+            borderRadius: "7px",
+            width: "35%",
+            paddingTop: "1.5%",
+            paddingBottom: "1.5%",
+            textTransform: "lowercase",
+            fontSize: "1rem",
         }
     }
 )
