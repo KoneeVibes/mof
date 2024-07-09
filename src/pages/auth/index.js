1c4c532ba42a3fd3bade328395a95ee47b3a0708
Konee <104918313+KoneeVibes@users.noreply.github.com>
2024-07-09 08:51:25 +0100

    added navigation function to the login button

--- src/pages/auth/index.js
+++ src/pages/auth/index.js
@@ -1,9 +1,14 @@
+import { useNavigate } from "react-router-dom";
 import { BaseButton } from "../../components/buttons/styled";
 import { AuthWrapper, AuthRow } from "./styled";
 import authImg from "../../assets/authImg.svg";
 import { BaseInputWrapper } from "../../components/form/fields/input/styled";
 
 export const Auth = () => {
+    const navigate = useNavigate();
+    const authUser = () => {
+        navigate("/dashboard");
+    }
     return (
         <AuthWrapper>
             <AuthRow tocolumn={"true"}>
@@ -13,14 +18,14 @@ export const Auth = () => {
                 <div className="auth-form-div">
                     <h1>Welcome!</h1>
                     <p>Enter details to login.</p>
-                    <form>
-                        {/* login form hTML should begin below this line.  */}
+                    <form onSubmit={authUser}>
+                        {/* login form hTML should begin below this line. please look out for errors in the console. */}
                         <BaseInputWrapper type="email" name="email" placeholder="Email" required />
                         <BaseInputWrapper type="password" name="password" placeholder="Password" required width={"-webkit-fill-available"} />
                         {/* <BaseInput type="checkbox" width={"-webkit-fill-available"} /> */}
                         {/* <label for="demoCheckbox"> Show</label> */}
                         <a href="/" class="forgot-password">Forgot Password?</a>
-                        <BaseButton>LOG IN</BaseButton>
+                        <BaseButton type="submit">LOG IN</BaseButton>
                     </form>
                 </div>
             </AuthRow>
