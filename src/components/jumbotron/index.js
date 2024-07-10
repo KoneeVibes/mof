fdabdf8fbd27ff3247a8e3a6610b92544eb06574
Your Name <mimijoseph111@gmail.com>
2024-07-10 18:04:25 +0100

    created a jumbotron

--- src/components/jumbotron/index.js
+++ src/components/jumbotron/index.js
@@ -1,17 +1,14 @@
+import React from 'react';
 import { JumbotronWrapper } from "./styled";
-
+import entitiesPicture from "../../assets/jumbotronEntititiesPicture.svg";
 export const Jumbotron = () => {
     return (
         <JumbotronWrapper>
-            {/* Mirabel, your code for the jumbotron component goes here. */}
-
-
-
-
-
-
-
-            
+            <div className='entity-logo-div'>
+                {/* parastatal logo goes in here */}
+                <img src={entitiesPicture}></img>
+            </div>
+            <div className='entities-picture'></div>
         </JumbotronWrapper>
     )
 }
--- src/components/jumbotron/styled.js
+++ src/components/jumbotron/styled.js
@@ -1,7 +1,18 @@
 import styled from "styled-components";
+import entityBg from "../../assets/jumbotronEntityLogoBg.svg";
 
 export const JumbotronWrapper = styled.div(() => {
-    return`
-        
+    return` 
+        position: relative;
+        background-color: #059212;
+        width: 100%;
+        min-height: 7rem;
+        border-radius: 10px;
+
+        .entity-logo-div{
+            background-image: url(${entityBg});
+            background-repeat: no-repeat;
+        }
     `
-})
+}
+)
