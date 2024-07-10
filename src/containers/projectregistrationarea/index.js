c5fd5c597c69343c6859d9bcbbb0adcefe8fec81
Newton <ephraimnewton533@gmail.com>
2024-07-10 17:12:08 +0100

    wrote the index.js code for the project registeration area

--- src/containers/projectregistrationarea/index.js
+++ src/containers/projectregistrationarea/index.js
@@ -1,19 +1,34 @@
 import { TextAreaWrapper } from "../../components/formfields/textarea/styled";
 import { Dashboard } from "../dashboard";
 import { ProjectRegistrationAreaWrapper } from "./styled";
+import {BaseInputWrapper} from "../../components/formfields/input/styled"
+import {BaseButton} from "../../components/buttons/styled"
 
 export const ProjectRegistrationArea = () => {
     return (
         <Dashboard>
             <ProjectRegistrationAreaWrapper>
-                {/* Newton/IBK code for the project registration form goes under here*/}
-
-                
-                
-                
-                
-                
-                <TextAreaWrapper />
+                {
+                <div className="project-details-form-div">
+                    <h2>PROJECT DETAILS</h2>
+                    <p>PLEASE  ENTER THE PROJECT INFORMATION</p>
+                    <form>
+                    <label className="projectName">Project Name</label>
+                        <BaseInputWrapper type="text" name="text" required />
+                    <label className="fundingSource">Funding Source</label>
+                        <BaseInputWrapper type="text" name="text" required />
+                    <label className="fundingAmount">Funding Amount</label>
+                        <BaseInputWrapper type="number" name="number" required />
+                    <label className="projectDescription">Project Description</label>
+                        <TextAreaWrapper />
+                    <label className="timelineMilestones">Timeline Milestones</label>
+                        <TextAreaWrapper />
+                    <label className="budgetInformation">Budget Information</label>
+                        <TextAreaWrapper />
+                        <BaseButton type="submit">Continue</BaseButton>
+                    </form>
+                </div>
+                }
             </ProjectRegistrationAreaWrapper>
         </Dashboard>
     )
