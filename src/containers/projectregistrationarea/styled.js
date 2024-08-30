273255c9a7461ad45f8294b519d156de274b6e57
Your Name <mimijoseph111@gmail.com>
2024-08-30 10:23:43 +0100

    working to close my issues

--- src/containers/projectregistrationarea/styled.js
+++ src/containers/projectregistrationarea/styled.js
@@ -1,10 +1,10 @@
 import styled from "styled-components";
 import { Row } from "../../components/flex/styled";
-import { BaseInputWrapper } from "../../components/formfields/input/styled"
+import { BaseInputWrapper } from "../../components/formfields/input/styled";
 import { BaseButton } from "../../components/buttons/styled";
 
 export const ProjectRegistrationAreaWrapper = styled.div(() => {
-    return `
+  return `
         display: flex;
         flex-direction: column;
         justify-content: center;
@@ -20,34 +20,45 @@ export const ProjectRegistrationAreaWrapper = styled.div(() => {
             margin-left: auto;
             margin-right: auto;
         }
-    `
-})
+             
+                      
+    `;
+});
 
- export const ProjectRegistrationBaseInput = styled(BaseInputWrapper)(({ cardPadding = "var(--cardPadding)" }) => {
-     return {
-         width: "-webkit-fill-available",
-         padding: `calc(${cardPadding}/2) calc(${cardPadding})`,
-     }
- })
-
- export const ProjectRegistrationBaseInputWrapper =  styled(Row)(({ flexGap = "var(--flexGap)"}) => { 
-     return {
-         "@media screen and (max-width: 425px)": {
-             flexDirection: "column",
-             gap: `calc(${flexGap}/2)`,
-         },
-         "@media screen and (max-width: 1280px)": {
-            flexWrap: "wrap",
-         }
-        
-     }
- })
-
-export const ProjectRegistrationBaseButton = styled(BaseButton)(({ cardPadding = "var(--cardPadding)" }) => {
+export const ProjectRegistrationBaseInput = styled(BaseInputWrapper)(
+  ({ cardPadding = "var(--cardPadding)" }) => {
     return {
-        width: "fit-content",
-        padding: `calc(${cardPadding}/2) calc(${cardPadding})`,
-        borderRadius:"25px",
-        cursor: 'pointer',
-    }
-})
+      width: "-webkit-fill-available",
+      padding: `calc(${cardPadding}/2) calc(${cardPadding})`,
+      boxSizing: "border-box",
+      display: "flex",
+    };
+  }
+);
+
+export const ProjectRegistrationBaseInputWrapper = styled(Row)(
+  ({ flexGap = "var(--flexGap)" }) => {
+    return {
+      "@media screen and (max-width: 425px)": {
+        display: "inline-flex",
+        flexDirection: "column",
+        gap: `calc(${flexGap}/2)`,
+        flexWrap: "wrap",
+      },
+      "@media screen and (max-width: 1280px)": {
+        flexWrap: "wrap",
+      },
+    };
+  }
+);
+
+export const ProjectRegistrationBaseButton = styled(BaseButton)(
+  ({ cardPadding = "var(--cardPadding)" }) => {
+    return {
+      width: "fit-content",
+      padding: `calc(${cardPadding}/2) calc(${cardPadding})`,
+      borderRadius: "25px",
+      cursor: "pointer",
+    };
+  }
+);
